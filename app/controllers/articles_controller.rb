ba8d35e01bce351281796cf513bff9f438880e62
Benoit Calin <benoit.calin@gmail.com>
2020-07-30 10:57:25 +0200

    Add Cloudinary && ActiveStorage for Article photo

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,7 @@
+class ArticlesController < ApplicationController
+
+  # Pour le Admin::ArticlesController :
+  # def article_params
+  #   params.require(:article).permit(:title, :content, :photo)
+  # end
+end
