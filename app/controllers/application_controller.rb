1923a479767d2a15ad8b5d8b108f667323113101
Benoit Calin <benoit.calin@gmail.com>
2020-10-23 11:31:02 +0200

    Add metadatas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # For metatags
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
